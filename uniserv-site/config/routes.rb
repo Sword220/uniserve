aacd113314ea43cf05b3baa347f4c5f1582ca0a3
Pete Sword <pete.sword@yahoo.com>
2018-08-27 17:56:57 -0600

    routes for controllers

--- uniserv-site/config/routes.rb
+++ uniserv-site/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    #API ROUTES SHOULD GO HERE
+    resources staffs:, only: [:index]
+    resources weas:, only: [:index]
+    resources oeas:, only: [:index]
   end
 
   #Do not place any routes below this one
