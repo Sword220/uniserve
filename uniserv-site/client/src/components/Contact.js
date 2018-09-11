9b042c92746e80059aca992823c6efce3840b141
Pete Sword <pete.sword@yahoo.com>
2018-09-11 11:40:00 -0600

    contact form rendered to page

--- uniserv-site/client/src/components/Contact.js
+++ uniserv-site/client/src/components/Contact.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import axios from 'axios'
 
 class Contact extends React.Component {
   render() {
     return(
-      <div>contact</div>
+      <div style={{ background: 'white' }}>
+      <div class="row">
+    <form class="col s12" style={{ textColor: 'black' }}>
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="first name" id="first_name" type="text" class="validate" />
+          <label style={{ color: 'black'}} class="active" for="first_name">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="last name" id="last_name" type="text" class="validate" />
+          <label class="active" for="last_name">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input placeholder="email" id="email" type="email" class="validate" />
+          <label class="active" for="email">Email</label>
+        </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input placeholder="###-####" id="phone" type="text" class="validate" />
+          <label class="active" for="phone">Phone Number</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea placeholder="message goes here" id="textarea1" class="materialize-textarea"></textarea>
+          <label class="active" for="textarea1">Textarea</label>
+        </div>
+      </div>
+      </div>
+      </form>
+     </div>
+    </div>
     )
   }
 }
