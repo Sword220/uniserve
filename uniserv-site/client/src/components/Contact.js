0c64dbc8b0b01fcffab71aef6c9b48d50325c1f1
Pete Sword <pete.sword@yahoo.com>
2018-09-13 16:19:38 -0600

    stylesheets, contact form functionality

--- uniserv-site/client/src/components/Contact.js
+++ uniserv-site/client/src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import axios from 'axios'
-
+import '../styles/contact.css'
 class Contact extends React.Component {
   state = { name: '', email: '', message: '' }
 
@@ -13,72 +13,33 @@ class Contact extends React.Component {
     const { name, email, message } = this.state
 
     return(
-      <div 
-        style={{   
-          background: 'white', 
-          height: '500px',
-          width: '800px',
-          display: 'flex',
-          alignItems: 'center',
-        }}>
-        <div class="row">
-          <form style={{ width: '800px', textColor: 'black' }}>
-            <div class="row">
-              <div class="input-field col s12">
-                <input 
-                  type="text" 
-                  class="validate" 
-                  name="name"
-                  value={name} 
-                  onChange={this.handleChange} 
-                />
-                <label 
-                  style={{ color: 'black'}} 
-                  class="active" 
-                  for="name"
-                >
-                  First and Last Name
-                </label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <input 
-                  type="email" 
-                  class="validate" 
-                  name="email"
-                  value={email}
-                  onChange={this.handleChange} 
-                />
-                <label 
-                  style={{ color: 'black' }}
-                  class="active" 
-                  for="email"
-                >
-                  Email
-                </label>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <textarea 
-                    class="materialize-textarea"
-                    name="message"
-                    value={message}
-                    onChange={this.handleChange}
-                  >
-                  </textarea>
-                  <label
-                    style={{ color: 'black' }} 
-                    class="active" 
-                    for="textarea1"
-                  >
-                    Textarea
-                  </label>
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
+      <div id="form_container">
+        <form id="form_style">
+          <label id="label_style" for="name">Name</label>
+          <input 
+            required
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+          />
+          <label id="label_style" for="email">Email</label>
+          <input
+            required
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+            <label id="label_style" for="textarea1">Textarea</label>
+            <textarea 
+              id="textarea1" 
+              class="materialize-textarea"
+              required
+              name="message"
+              value={message}
+              onChange={this.handleChange}
+            >
+            </textarea>
+        </form>
       </div>
     )
   }
